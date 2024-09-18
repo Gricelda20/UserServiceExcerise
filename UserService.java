1a475a7ae03719ca268b92b21511fa7dd3b94b8c
Gricelda Padilla <114915430+Gricelda20@users.noreply.github.com>
2024-09-18 11:08:54 -0400

    Initial commit

---
+++ UserService.java
@@ -0,0 +1,17 @@
+package UserServiceExcerise;
+
+	public class UserService {
+
+	    // Method to create a User object from an array of Strings
+	    public UserService createUser(String[] stringInput) {
+	        if (stringInput == null || stringInput.length != 3) {
+	
+	        }
+
+	        return new UserService();
+	    }
+	}
+	
+	
+
+
